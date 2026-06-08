2d7efcbf81201560e66f960878d46105eb1b1000
Arav Kumar Yadav <aravkryadav2005@gmail.com>
2026-06-08 22:02:03 +0530

    06 Template Selecting Modal

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // for Google profile images
+      },
+    ],
+  },
 };
 
 export default nextConfig;
