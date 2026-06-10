ee47912faf29f6b55c967da1f7b1a5db98610a89
Arav Kumar Yadav <aravkryadav2005@gmail.com>
2026-06-10 13:00:48 +0530

    10-Webcontainers-part-1

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,23 @@
 
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "avatars.githubusercontent.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "lh3.googleusercontent.com", // for Google profile images
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -6,14 +25,31 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com", // for Google profile images
+        hostname: "*",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Apply to all routes
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
